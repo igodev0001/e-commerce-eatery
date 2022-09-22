d96e8dae9abe835f188daacf4206eebf063049b9
azuredeviil053 <azuredevil053@gmail.com>
2022-09-22 15:15:10 +0330

    dynamic route added

--- components/download_app/DownloadApp.tsx
+++ components/download_app/DownloadApp.tsx
@@ -4,19 +4,36 @@ import FeedWide from "../feed/FeedWide";
 import Dl_Component from "./Dl_Component";
 
 import styles from "../../styles/DownloadApp.module.scss";
+import useFetch from "../../hooks/useFetch";
+
+interface Feed {
+  id: number;
+  title: string;
+  desc: string;
+  legal: string;
+  image: string;
+  linkName: string;
+  href: string;
+}
 
 export default function Dl() {
+  const feeds = useFetch("download_app");
   return (
     <div className={styles.container}>
       <h1>McDonald&apos;s App</h1>
-      <FeedWide
-        title={item.title}
-        desc={item.desc}
-        legal={item.legal}
-        image={item.image}
-        linkName={item.linkName}
-        href={item.href}
-      />
+
+      {feeds.data.map((item: Feed) => (
+        <FeedWide
+          key={item.id}
+          title={item.title}
+          desc={item.desc}
+          legal={item.legal}
+          image={item.image}
+          linkName={item.linkName}
+          href={item.href}
+        />
+      ))}
+
       <Dl_Component />
       <div className={styles.desc}>
         <h2>Deals and Much More</h2>
