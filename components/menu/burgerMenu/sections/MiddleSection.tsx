74d5e6bdb122ad411726841d29e1050f54991c6e
azuredeviil053 <azuredevil053@gmail.com>
2022-11-02 16:36:41 +0330

    menu added to burger

--- components/menu/burgerMenu/sections/MiddleSection.tsx
+++ components/menu/burgerMenu/sections/MiddleSection.tsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Sections.module.scss";
+import MenuList from "../../popup_menu/MenuList";
+import useCategory from "../../../../hooks/useCategory";
 
 export default function MiddleSection() {
+  const [isShow, setIsShow] = useState(false);
+  const { data } = useCategory();
+
+  function menuHandle() {
+    setIsShow((prev) => !prev);
+  }
+
   return (
     <nav className={styles.middle}>
       <ul>
-        <li>Our Menu</li>
+        <li>
+          <button onClick={menuHandle}>Our Menu</button>
+          <div>{isShow && <MenuList categories={data} form="burger" />}</div>
+        </li>
         <li>
           <Link href="/download_app">Download App</Link>
         </li>
--- components/menu/popup_menu/MenuList.tsx
+++ components/menu/popup_menu/MenuList.tsx
@@ -1,12 +1,16 @@
 import MenuItem from "./MenuItem";
 //===================================================
-import { Categories } from "../../../lib/types";
+import { Category } from "../../../lib/types";
 import styles from "./PopupMenu.module.scss";
 //===================================================
-
-export default function MenuList({ categories }: Categories) {
+interface Props {
+  categories: Category[];
+  form: string;
+}
+export default function MenuList({ categories, form }: Props) {
+  const listClass = form === "popUp" ? "listPopUp" : "listBurger";
   return (
-    <ul className={styles.menuList}>
+    <ul className={` ${styles[listClass]}`}>
       {categories.map(
         (item) =>
           item.name !== "Favorites" && (
