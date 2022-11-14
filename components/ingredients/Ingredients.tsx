f8e00d75203c6075acb53f8ecabddede0c09cdbd
azuredeviil053 <azuredevil053@gmail.com>
2022-11-14 10:51:03 +0330

    ingredient responsive Done

--- components/ingredients/Ingredients.tsx
+++ components/ingredients/Ingredients.tsx
@@ -1,6 +1,7 @@
 //======================================================
 import IngredientsGrid from "./IngredientsGrid";
 import MobileStepperSwip from "./MobileStepperSwip";
+import useWindowSize from "../../hooks/useWindowSize";
 //======================================================
 import styles from "./Ingredients.module.scss";
 import { Ingredient } from "../../lib/types";
@@ -11,11 +12,15 @@ interface Props {
   name: string;
 }
 export default function Ingredients({ ingredients, name }: Props) {
+  const { width } = useWindowSize();
   return (
     <div className={styles.container}>
       <h2>{`Ingredients in the ${name}`}</h2>
-      <IngredientsGrid ingredients={ingredients} />
-      <MobileStepperSwip ingredients={ingredients} />
+      {width > 1023 ? (
+        <IngredientsGrid ingredients={ingredients} />
+      ) : (
+        <MobileStepperSwip ingredients={ingredients} />
+      )}
     </div>
   );
 }
